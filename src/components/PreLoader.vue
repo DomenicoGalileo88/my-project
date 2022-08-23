e2d4d0aa2074b582843c0d9fb74f38e176bff28b
DomenicoGalileo88 <domenico.galileo@gmail.com>
2022-08-23 18:47:48 +0200

    Add avatar preloader

--- src/components/PreLoader.vue
+++ src/components/PreLoader.vue
@@ -4,6 +4,11 @@
       <div class="logo">
         <!-- <img alt="Vue logo" src="@/assets/logo.png" /> -->
       </div>
+
+      <div class="avatar">
+        <img width="200" src="@/assets/img/avatar-domenico.png" alt="avatar" />
+      </div>
+
       <!-- <p>CIao sono un loader, almeno credo</p> -->
       <h1 class="animate__animated animate__lightSpeedInLeft logo_name mb-5">
         Domenico Galileo
@@ -69,6 +74,10 @@ export default {
     background-repeat: no-repeat;
     --animate-delay: 1s;
   }
+  .avatar {
+    position: relative;
+    bottom: 160px;
+  }
   .wrapper {
     width: 100%;
     height: 50px;
@@ -107,10 +116,9 @@ export default {
     background-color: #000000;
     background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 800 400'%3E%3Cdefs%3E%3CradialGradient id='a' cx='396' cy='281' r='514' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23AE4119'/%3E%3Cstop offset='1' stop-color='%23330000'/%3E%3C/radialGradient%3E%3ClinearGradient id='b' gradientUnits='userSpaceOnUse' x1='400' y1='148' x2='400' y2='333'%3E%3Cstop offset='0' stop-color='%23FA3' stop-opacity='0'/%3E%3Cstop offset='1' stop-color='%23FA3' stop-opacity='0.5'/%3E%3C/linearGradient%3E%3C/defs%3E%3Crect fill='url(%23a)' width='800' height='400'/%3E%3Cg fill-opacity='0.4'%3E%3Ccircle fill='url(%23b)' cx='267.5' cy='61' r='300'/%3E%3Ccircle fill='url(%23b)' cx='532.5' cy='61' r='300'/%3E%3Ccircle fill='url(%23b)' cx='400' cy='30' r='300'/%3E%3C/g%3E%3C/svg%3E");
     background-attachment: fixed;
-    background-size: cover; 
+    background-size: cover;
   }
   100% {
-    
     background-color: #330000;
     background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 800 400'%3E%3Cdefs%3E%3CradialGradient id='a' cx='396' cy='281' r='514' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23AE4119'/%3E%3Cstop offset='1' stop-color='%23330000'/%3E%3C/radialGradient%3E%3ClinearGradient id='b' gradientUnits='userSpaceOnUse' x1='400' y1='148' x2='400' y2='333'%3E%3Cstop offset='0' stop-color='%23FA3' stop-opacity='0'/%3E%3Cstop offset='1' stop-color='%23FA3' stop-opacity='0.5'/%3E%3C/linearGradient%3E%3C/defs%3E%3Crect fill='url(%23a)' width='800' height='400'/%3E%3Cg fill-opacity='0.4'%3E%3Ccircle fill='url(%23b)' cx='267.5' cy='61' r='300'/%3E%3Ccircle fill='url(%23b)' cx='532.5' cy='61' r='300'/%3E%3Ccircle fill='url(%23b)' cx='400' cy='30' r='300'/%3E%3C/g%3E%3C/svg%3E");
     background-attachment: fixed;
